59a24ff03f105fb67325d0ff88ca48c0d62c60db
Juan Miguel Austria <juanmiguelplaysmc@gmail.com>
2025-08-05 11:57:44 +0800

    update button toggle darklight

--- Jm_WebPortfolio/JM_Portfolio.js
+++ Jm_WebPortfolio/JM_Portfolio.js
@@ -26,5 +26,22 @@
    }, 3000);
 });
 
+const toggleButton = document.getElementById("toggle-button");
+const indicator = document.querySelector(".indicator");
+const featherIcon = document.querySelector(".feather");
+const featherSunIcon = document.querySelector(".feather-sun");
+/*
+const bodyToggle = document.querySelector("body");
+const headerToggle = document.querySelector(".Header1");
+const mainNavLinks = document.querySelectorAll(".main-nav a");
+*/
+
+toggleButton.onclick = () => {
+    toggleButton.classList.toggle("active");
+    indicator.classList.toggle("active");
+    featherIcon.classList.toggle("active");
+    featherSunIcon.classList.toggle("active");
+   
+}
 
 
